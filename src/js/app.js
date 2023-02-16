4e5655ad14c060dd66d31859a964d8ef5d34518a
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-02-16 20:43:37 +0000

    uploaded

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>"${variables.name ? variables.name : "Nombre"} ${
+    variables.lastname ? variables.lastname : "Apellido"
+  }"</h1>
+          <h2>"${variables.role ? variables.role : "Ocupación"}"</h2>
+          <h3>"${variables.city ? variables.city : "Ciudad"}, ${
+    variables.country ? variables.country : "País"
+  }",</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter ? variables.twitter : "#"
+            }" style = "color : ${
+    variables.twitter ? "white" : "grey"
+  };"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github ? variables.github : "#"
+            }" style = "color : ${
+    variables.github !== "alesanchezr" ? "white" : "grey"
+  };"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin ? variables.linkedin : "#"
+            }" style ="color: ${
+    variables.linkedin ? "white" : "grey"
+  };"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=""${
+              variables.instagram ? variables.instagram : "#"
+            }" style = "color: ${
+    variables.instagram ? "white" : "grey"
+  };"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
